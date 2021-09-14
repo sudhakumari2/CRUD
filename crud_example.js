f729abe927c7612ec15cbd75af94ca454d3376b0
sudhakumari2 <75968597+sudhakumari2@users.noreply.github.com>
2021-09-14 15:53:52 +0530

    Add files via upload

---
+++ crud_example.js
@@ -0,0 +1,41 @@
+const knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host : '127.0.0.1',
+      port : 3306,
+      user : 'root',
+      password : 'Sudha@123',
+      database : 'sudhakumari'
+    }
+  });
+
+// we are inserting the data in the table
+knex('user').insert({ id: 8, first_name: 'shanti', last_name: 'kumari' })
+.then((data)=>{
+    console.log(data);
+}).catch((err)=>{
+    console.log(err)
+})
+
+// we are gettting the data from table 
+knex.select('*').from('user')
+.then((data)=>{
+    // console.log(data)
+}).catch((err)=>{
+    // console.log(err)
+})
+
+// update the data in table
+knex.update({id:10,first_name: 'anchal', last_name: 'prasad'})
+.table('user').where("id",7).then((data)=>{
+    // console.log("updating");
+}).catch((err)=>{
+    // console.log("not updating")
+})
+
+// delete the data from table
+knex('user').where('id',9).del().then((data)=>{
+    // console.log("delete")
+}).catch((err)=>{
+    // console.log(err)
+})
